aa667eadeea0a1b226170f17d5fecb4148f760d0
jdtavarez <juliodtavarez@gmail.com>
2022-03-04 12:04:35 -0500

    on reactions

--- aws.py
+++ aws.py
@@ -1,9 +1,3 @@
 import boto3
 
 dynamodb = boto3.client('dynamodb')
-
-response = dynamodb.describe_table(
-    TableName='jobsbot'
-)
-
-print(response)
--- bot.py
+++ bot.py
@@ -21,7 +21,22 @@ async def on_ready():
         f'{guild.name} (id: {guild.id} )'
     )
 
-    members = '\n - '.join([member.display_name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    for channel in guild.channels: 
+        if channel.name == "jobs":
+            break
+
+    print (
+        f'{channel.id}'
+    )
+
+    await channel.send("Hello there... I'm the jobs bot")
+
+@client.event
+async def on_raw_reaction_add(payload):
+    channel = await client.fetch_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    user = await client.fetch_user(payload.user_id)
+
+    await user.send("hi I see you reacted to my message")
 
 client.run(TOKEN)
